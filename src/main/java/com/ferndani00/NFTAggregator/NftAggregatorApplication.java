223bc277d521b8badd9fb83f036cdbb4bb103981
Daniel <ferndani00@gmail.com>
2024-02-18 20:45:40 +0100

    Initial commit

---
+++ src/main/java/com/ferndani00/NFTAggregator/NftAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.ferndani00.NFTAggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferndani00/NFTAggregator/NftAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferndani00.NFTAggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
