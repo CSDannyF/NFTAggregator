fadc06fa43959424b7976fc6d3f27eb8e471ffce
Daniel <ferndani00@gmail.com>
2024-03-12 23:19:08 +0100

    Created controller for nftcards

--- src/main/java/com/ferndani00/NFTAggregator/controllers/NFTCardController.java
+++ src/main/java/com/ferndani00/NFTAggregator/controllers/NFTCardController.java
@@ -1,2 +1,17 @@
-package com.ferndani00.NFTAggregator.controllers;public class NFTCardController {
+package com.ferndani00.NFTAggregator.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NFTCardController {
+
+    @GetMapping("/nftCard")
+    public String showCards(Model model)
+    {
+        //List<NFT> nfts = nftService.getAll();
+        //model.addAttribute("nfts", nfts);
+        return "nftCard";
+    }
 }
